095110efee99f232a123b48a27e5634cc1cdb5c9
Mahmoud Mostafa <m_devolepment@hotmail.com>
2025-02-13 04:21:32 +0200

    Edit scrolToTop file

--- src/shared/ScrollToTop.jsx
+++ src/shared/ScrollToTop.jsx
@@ -1,32 +1,14 @@
-import { useEffect, useRef } from 'react';
 import { ArrowUp } from '@phosphor-icons/react';
 
 function ScrollToTop() {
-  const scrollBtnEl = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 150) {
-        scrollBtnEl.current.classList.add('show');
-      } else {
-        scrollBtnEl.current.classList.remove('show');
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
     <button
-      ref={scrollBtnEl}
       className="btn btn__primary btn__scroll-top"
       onClick={() => window.scrollTo(0, 0)}
       aria-label="Scroll to top"
       data-aos="fade-up"
       data-aos-once="false"
-      data-aos-offset="100"
+      data-aos-offset="600"
     >
       <ArrowUp size={28} />
     </button>
